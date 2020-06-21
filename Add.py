5702b3f7c1426aea3ab77de2da2ea810b29f19b2
Shuaib6428 <66258933+Shuaib6428@users.noreply.github.com>
2020-06-21 22:31:50 +0530

    Add files via upload

---
+++ Add.py
@@ -0,0 +1,34 @@
+from tkinter import *
+root=Tk()
+root.title("Addition program")
+root.geometry('330x290')
+root.wm_minsize(width=330,height=290)
+root.wm_maxsize(width=330,height=290)
+
+def sum():
+    a=int(e1.get())
+    b=int(e2.get())
+    s=a+b
+    l4.config(text="sum of %d and %d is %d" % (a,b,s))
+
+l1=Label(root,font=('calibri',15,'bold'))
+l1.config(text='Addition of two Number')
+l1.place(x=62,y=10)
+
+l2=Label(root,text='first Number ',font=('calibri',12))
+l2.place(x=10,y=80)
+e1=Entry(root,font=('calibri',12),borderwidth=2,relief='solid')
+e1.place(x=140,y=80)
+
+l3=Label(root,text='second Number',font=('calibri',12))
+l3.place(x=10,y=120)
+e2=Entry(root,font=('calibri',12),borderwidth=2,relief='solid')
+e2.place(x=140,y=120)
+
+b1=Button(root,text="ADD",font=('calibri',10),bg='white',borderwidth=2,relief='solid',command=sum)
+b1.place(x=100,y=170,width=80,height=30)
+
+l4=Label(root,text='Result will soon',font=('calibri',12))
+l4.place(x=20,y=240)
+
+root.mainloop()
