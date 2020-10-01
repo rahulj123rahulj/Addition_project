9f31f3b24c32b62a6a3aa5f0523c60c5da5c90bc
rahulj123rahulj <68708719+rahulj123rahulj@users.noreply.github.com>
2020-10-01 12:51:26 +0530

    Update Add.py

--- Add.py
+++ Add.py
@@ -1,6 +1,6 @@
 from tkinter import *
 root=Tk()
-root.title("Addition program")
+root.title("Addition and multiplication")
 root.geometry('330x290')
 root.wm_minsize(width=330,height=290)
 root.wm_maxsize(width=330,height=290)
@@ -10,6 +10,12 @@ def sum():
     b=int(e2.get())
     s=a+b
     l4.config(text="sum of %d and %d is %d" % (a,b,s))
+
+def mul():
+    a=int(e1.get())
+    b=int(e2.get())
+    m=a*b
+    l4.config(text="Multiplication of %d and %d is %d"%(a,b,m))
 
 l1=Label(root,font=('calibri',15,'bold'))
 l1.config(text='Addition of two Number')
@@ -27,6 +33,9 @@ e2.place(x=140,y=120)
 
 b1=Button(root,text="ADD",font=('calibri',10),bg='white',borderwidth=2,relief='solid',command=sum)
 b1.place(x=100,y=170,width=80,height=30)
+
+b2=Button(root,text="MULTIPLY",font=('calibri',10),bg='white',borderwidth=2,relief='solid',command=mul)
+b2.place(x=200,y=170,width=80,height=30)
 
 l4=Label(root,text='Result will soon',font=('calibri',12))
 l4.place(x=20,y=240)
